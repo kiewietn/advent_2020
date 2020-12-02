c66f6021e34dd684c150b68d8f197b97b6d92c4c
Nico Kiewiet <ngkiewiet@gmail.com>
2020-12-02 17:29:22 +0200

    day2

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -17,5 +17,45 @@
           z vals
           :when (= 2020 (+ x y z))] (* x y z))))
 
+(defn parse-key-range [key-range]
+  (let [[valid-range value] (clojure.string/split key-range #" ")
+        [lower-bound upper-bound] (clojure.string/split valid-range #"-")]
+    [[(Integer. lower-bound) (Integer. upper-bound)] value]))
+
+(defn parse-password [composed-password]
+  (let [[key-range key-val]
+        (map #(clojure.string/trim %1) (clojure.string/split composed-password #":"))
+        parsed-key-range (parse-key-range key-range)]
+    (list parsed-key-range key-val)))
+
+(defn valid-password? [parsed-password]
+  (let [lower-bound (nth (first (first parsed-password)) 0)
+        upper-bound (nth (first (first parsed-password)) 1)
+        test-char (first (first (rest (first parsed-password))))
+        password (first (rest parsed-password))
+        character-frequency (count (filter #(= test-char %) password))]
+    (and (<= character-frequency upper-bound)
+         (>= character-frequency lower-bound))))
+
+(defn valid-password2? [parsed-password]
+  (let [lower-index (dec (nth (first (first parsed-password)) 0))
+        upper-index (dec (nth (first (first parsed-password)) 1))
+        test-char (first (first (rest (first parsed-password))))
+        password (vec (first (rest parsed-password)))]
+    (and (or (= test-char (nth password lower-index))
+         (= test-char (nth password upper-index)))
+         (not (and (= test-char (nth password upper-index))
+                   (= test-char (nth password lower-index)))))))
+
+(defn day2_1 [password-combos]
+  (let [parsed-passwords (map #(parse-password %) password-combos)
+        valid-passwords (filter #(valid-password? %) parsed-passwords)]
+    (count valid-passwords)))
+
+(defn day2_2 [password-combos]
+  (let [parsed-passwords (map #(parse-password %) password-combos)
+        valid-passwords (filter #(valid-password2? %) parsed-passwords)]
+    (count valid-passwords)))
+
 (defn -main [& args]
-  (println "hello moo! " (day1_2)))
+  (println "hello moo! " (day2_2 (split-file "resources/day2.input"))))
