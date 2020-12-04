703605e8867c52e4ba6ba70a454e349fabea311b
Nico Kiewiet <ngkiewiet@gmail.com>
2020-12-04 12:57:58 +0200

    day4

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -71,6 +71,60 @@
 (defn day3_2 [test-data]
   (reduce * (map #(day3_1 test-data (first %1) (first (rest %1))) '((1 1) (1 3) (1 5) (1 7) (2 1)))))
 
+(defn parse-passports [raw-data]
+        (let [passport-strings (map #(clojure.string/replace % #"\n" " ") (clojure.string/split raw-data #"\n\n"))]
+          (map #(reduce (fn [a-map key-val]
+                          (let [[a-key a-val] (clojure.string/split key-val #":")]
+                            (assoc a-map a-key a-val))) {} %) (map #(clojure.string/split % #" ") passport-strings))))
+
+(defn day4_1 [raw-data]
+  (count (filter #(every? (dissoc % "cid") '("byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid")) (parse-passports raw-data))))
+
+(defn valid-birth-year? [birth-year]
+        (cond (= 4 (count birth-year))
+              (let [as-number (Integer. birth-year)]
+                (and (>= as-number 1920) (<= as-number 2002)))
+              :else false))
+
+(defn valid-issue-year? [issue-year]
+        (cond (= 4 (count issue-year))
+              (let [as-number (Integer. issue-year)]
+                (and (>= as-number 2010) (<= as-number 2020)))
+              :else false))
+
+(defn valid-expiration-year? [expiration-year]
+        (cond (= 4 (count expiration-year))
+              (let [as-number (Integer. expiration-year)]
+                (and (>= as-number 2020) (<= as-number 2030)))
+              :else false))
+
+(defn valid-height? [height]
+        (cond (clojure.string/ends-with? height "cm")
+              (let [as-number (Integer. (clojure.string/replace height "cm" ""))]
+                (and (>= as-number 150) (<= as-number 193)))
+              (clojure.string/ends-with? height "in")
+              (let [as-number (Integer. (clojure.string/replace height "in" ""))]
+                (and (>= as-number 59) (<= as-number 76)))
+              :else false))
+
+(defn valid-hcl? [color]
+  (re-matches #"^#[a-z0-9]{6}" color))
+
+(defn valid-eye-color? [color]
+  (some #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"} (list color)))
+
+(defn valid-pid? [pid]
+  (re-matches #"[0-9]{9}" pid))
+
+(defn day4_2 [raw-data]
+  (let [passport-candidates (filter #(every? (dissoc % "cid") '("byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid")) (parse-passports raw-data))]
+    (count (filter #(and (valid-birth-year? (get % "byr"))
+                   (valid-expiration-year? (get % "eyr"))
+                   (valid-eye-color? (get % "ecl"))
+                   (valid-hcl? (get % "hcl"))
+                   (valid-height? (get % "hgt"))
+                   (valid-issue-year? (get % "iyr"))
+                   (valid-pid? (get % "pid"))) passport-candidates))))
 
 (defn -main [& args]
-  (println "hello moo! " (day3_2 (split-file "resources/day3.input"))))
+  (println "hello moo! " (day4_2 (slurp "resources/day4.input"))))
